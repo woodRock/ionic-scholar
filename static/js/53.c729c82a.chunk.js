4954c34c2de7cde9fe333475421d5ffe4993ce94
woodRock <woodRock@users.noreply.github.com>
2021-03-06 03:26:31 +0000

    Deploying to master from @ woodRock/ionic-scholar@c96062a421e0dd5b5595557034e708148db45580 🚀

---
+++ static/js/53.c729c82a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpscholar=this.webpackJsonpscholar||[]).push([[53],{580:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return c}));var e=o(8),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.j)((function(){var t=n.innerWidth,o=n.innerHeight,c=document.elementFromPoint(t/2,o/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(e.g)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.c729c82a.chunk.js.map
